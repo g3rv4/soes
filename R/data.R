baf7f8d12d740e24637bf788acb998f679468ad4
Gervasio Marchand <gmc@gmc.uy>
2017-07-22 23:06:54 -0300

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,55 @@
+#' Badges
+#'
+#' Badges de los usuarios
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"badges"
+
+#' Comments
+#'
+#' Comentarios!
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"comments"
+
+#' Post History
+#'
+#' Historia de los posts
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"post_history"
+
+#' Post Links
+#'
+#' Links entre posts
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"post_links"
+
+#' Posts
+#'
+#' Posts
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"posts"
+
+#' Tags
+#'
+#' Tags
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"tags"
+
+#' Users
+#'
+#' Usuarios
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"users"
+
+#' Votes
+#'
+#' Votos
+#'
+#' @format Un data frame (TODO: Agregar datos del esquema)
+"votes"
